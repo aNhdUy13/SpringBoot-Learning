904216d6ed54a7f0cba22d44668910acbd4d68ed
Anh Nguyen Duy <anh.nguyen56@ntq-solution.com.vn>
2024-01-18 09:37:32 +0700

    refactor folder

--- src/main/java/com/example/demo/server/api/GetBookByNameApi.java
+++ src/main/java/com/example/demo/server/api/GetBookByNameApi.java
@@ -1,9 +1,12 @@
 package com.example.demo.server.api;
 
 import com.example.demo.server.response.ApplicationResponseBody;
+import com.example.demo.server.response.GetAllBookResponse;
 import com.example.demo.server.service.BookHandlerService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Component
@@ -12,6 +15,7 @@ public class GetBookByNameApi {
     private final BookHandlerService bookHandlerService;
 
     public ApplicationResponseBody doExecute(String bookName) {
-        return new ApplicationResponseBody(bookHandlerService.getBookByName(bookName));
+        List<GetAllBookResponse> bookResponseList = bookHandlerService.getBookByName(bookName);
+        return new ApplicationResponseBody(bookResponseList);
     }
 }
